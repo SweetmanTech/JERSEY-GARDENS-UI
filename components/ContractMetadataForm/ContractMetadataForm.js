1aff1d996bc1a773f751e84fb92f962e54711041
Sweets Sweetman <sweetmantech@gmail.com>
2022-08-22 16:17:47 -0300

    I remove unused state.

--- components/ContractMetadataForm/ContractMetadataForm.js
+++ components/ContractMetadataForm/ContractMetadataForm.js
@@ -1,10 +1,8 @@
-import { Button, Input } from 'degen'
+import { Input } from 'degen'
 import { useMusicMetadata } from 'music-metadata-ipfs'
 import { useCreateDrop } from '@providers/CreateDropProvider'
-import { useState } from 'react'
 
 const ContractMetadataForm = () => {
-  const [open, setOpen] = useState(false)
   const { contractMetadata, setContractMetadata } = useMusicMetadata()
   const {
     symbol,
@@ -19,69 +17,58 @@ const ContractMetadataForm = () => {
     setFundsRecipient,
   } = useCreateDrop()
 
-  const handleClick = () => {
-    setOpen(!open)
-  }
-
   return (
     <>
-      <Button width="full" variant="secondary" onClick={handleClick}>
-        Contract Metadata
-      </Button>
-      {open && (
-        <>
-          <Input
-            placeholder="drop name"
-            label="contract name"
-            onChange={(e) =>
-              setContractMetadata({ ...contractMetadata, name: e.target.value })
-            }
-          />
-          <Input
-            placeholder={symbol}
-            label="contract symbol"
-            onChange={(e) => setSymbol(e.target.value)}
-          />
-          <Input
-            placeholder={defaultAdmin}
-            label="default admin"
-            onChange={(e) => setDefaultAdmin(e.target.value)}
-          />
-          <Input
-            placeholder={editionSize}
-            label="edition size"
-            type="number"
-            step="1"
-            min={0}
-            onChange={(e) => setEditionSize(e.target.value)}
-          />
-          <Input
-            placeholder={royaltyBps}
-            label="royalty bps"
-            type="number"
-            step="1"
-            min={0}
-            onChange={(e) => {
-              setRoyaltyBps(e.target.value)
-              setContractMetadata({
-                ...contractMetadata,
-                seller_fee_basis_points: e.target.value,
-              })
-            }}
-          />
-          <Input
-            placeholder={fundsRecipient}
-            label="funds recipient"
-            onChange={(e) => {
-              setFundsRecipient(e.target.value)
-              setContractMetadata({
-                ...contractMetadata,
-                fee_recipient: e.target.value,
-              })
-            }}
-          />
-        </>
-      )}
+      <Input
+        placeholder="drop name"
+        label="contract name"
+        onChange={(e) =>
+          setContractMetadata({ ...contractMetadata, name: e.target.value })
+        }
+      />
+      <Input
+        placeholder={symbol}
+        label="contract symbol"
+        onChange={(e) => setSymbol(e.target.value)}
+      />
+      <Input
+        placeholder={defaultAdmin}
+        label="default admin"
+        onChange={(e) => setDefaultAdmin(e.target.value)}
+      />
+      <Input
+        placeholder={editionSize}
+        label="edition size"
+        type="number"
+        step="1"
+        min={0}
+        onChange={(e) => setEditionSize(e.target.value)}
+      />
+      <Input
+        placeholder={royaltyBps}
+        label="royalty bps"
+        type="number"
+        step="1"
+        min={0}
+        onChange={(e) => {
+          setRoyaltyBps(e.target.value)
+          setContractMetadata({
+            ...contractMetadata,
+            seller_fee_basis_points: e.target.value,
+          })
+        }}
+      />
+      <Input
+        placeholder={fundsRecipient}
+        label="funds recipient"
+        onChange={(e) => {
+          setFundsRecipient(e.target.value)
+          setContractMetadata({
+            ...contractMetadata,
+            fee_recipient: e.target.value,
+          })
+        }}
+      />
     </>
   )
 }
